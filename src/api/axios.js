66cf95b4d5c250ca7b564dd5bb2433ec41bafe03
iimagii <mag_1@ukr.net>
2023-05-31 21:11:42 +0300

    fix deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 import { getContentType } from '../utils/api.utils'
 
-const API_URL = 'http://localhost:7000/api'
+const API_URL = 'http://https://test-elfy.onrender.com/api'
 
 export const axiosClassic = axios.create({
   baseURL: API_URL,
